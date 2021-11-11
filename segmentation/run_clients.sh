5e67219a6d695ce23e1d4bc9665e2aa04b4eeff8
Przemek <p.jablecki@sanoscience.org>
2021-11-11 14:39:14 +0100

    Fix the script triggering clients

--- segmentation/run_clients.sh
+++ segmentation/run_clients.sh
@@ -2,9 +2,12 @@
 
 
 echo "server node: $1"
+i=0
 
-for i in {1..$2}
-do 
-    echo "starting $i/$2 client"
-    sbatch client.sh $1 $i $2
+while [ "$i" -lt "$2" ]; do
+	echo "starting $i/$2 client"
+	sbatch client.sh $1 $i $2
+	i=$((i + 1))
 done
+
+ 
