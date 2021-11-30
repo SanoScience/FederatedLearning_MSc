bcd754d4cc92d844e9132e51f5e8b5d0e06d477c
Przemysław Jabłecki <prz.jab98@gmail.com>
2021-11-30 17:11:41 +0100

    Generate patches

--- segmentation/manual_verification.py
+++ segmentation/manual_verification.py
@@ -8,6 +8,37 @@ from segmentation.data_loader import LungSegDataset
 from segmentation.models.unet import UNet
 import numpy as np
 
+
+def trim_ranges(l, r, bound):
+    if l < 0:
+        r += abs(l)
+        l = 0
+    if r >= bound:
+        l -= (bound - r)
+        r = bound - 1
+    return l, r
+
+
+def generate_patches(masked_image, K=10, patch_size=128):
+    w, h = masked_image.shape
+    shift = patch_size // 2
+    patches = []
+    fig = plt.figure(figsize=(10, 10))
+    cols = K // 2
+    rows = K // cols
+    for j in range(K):
+        x, y = np.where(masked_image > 0.5)
+        i = np.random.randint(len(x))
+        l_x, r_x = trim_ranges(x[i] - shift, x[i] + shift, w)
+        l_y, r_y = trim_ranges(y[i] - shift, y[i] + shift, h)
+        patches.append(masked_image[l_x:r_x, l_y:r_y])
+        fig.add_subplot(rows, cols, j + 1)
+        plt.imshow(patches[-1])
+        plt.title(str(j))
+    plt.show()
+    return list(map(lambda item: torch.from_numpy(item), patches))
+
+
 DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 print(DEVICE)
 BATCH_SIZE = 1
@@ -33,10 +64,10 @@ criterion = DiceLoss()
 net.eval()
 val_running_loss = 0.0
 val_running_jac = 0.0
+
 for batch_idx, (images, masks) in enumerate(test_loader):
     images = images.to(DEVICE)
     masks = masks.to(DEVICE)
-
     outputs_masks = net(images)
     # loss_seg = criterion(outputs_masks, masks)
     # loss = loss_seg
@@ -55,6 +86,7 @@ for batch_idx, (images, masks) in enumerate(test_loader):
     res = torch.cat((img, mask, out, torch.from_numpy(superposed)), 1).cpu().detach()
     plt.imshow(res)
     plt.show()
+    generate_patches(superposed)
 
 val_loss = val_running_loss / len(test_loader)
 val_jac = val_running_jac / len(test_loader)
