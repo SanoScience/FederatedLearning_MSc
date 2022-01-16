8c50edb5951b5e949446e739950a74ef9d2b1eb3
Przemysław Jabłecki <prz.jab98@gmail.com>
2022-01-16 22:38:08 +0100

    Use gpu and use paths to resources on Prometheus

--- classification/visualise/grad_cam.py
+++ classification/visualise/grad_cam.py
@@ -6,12 +6,13 @@ import torchvision
 from torch.autograd import Variable
 import matplotlib.pyplot as plt
 import numpy as np
-
+import pandas as pd
+import os
 import PIL
 import time
 
 st = time.time()
-device = torch.device('cpu')
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 
 def get_resnet_cam(model_path):
@@ -21,7 +22,7 @@ def get_resnet_cam(model_path):
         torch.load(model_path, map_location=device))
     target_layer_resnet = model.layer4[-1]
     cam = GradCAM(model=model, target_layers=[target_layer_resnet], use_cuda=False)
-    return cam, model
+    return cam, model.to(device)
 
 
 def get_densenet_cam(model_path):
@@ -30,8 +31,8 @@ def get_densenet_cam(model_path):
     model.load_state_dict(
         torch.load(model_path, map_location=device))
     target_layer_densenet = model.features[-1]
-    cam = GradCAM(model=model, target_layers=[target_layer_densenet], use_cuda=False)
-    return cam, model
+    cam = GradCAM(model=model, target_layers=[target_layer_densenet], use_cuda=True)
+    return cam, model.to(device)
 
 
 def get_image(path):
@@ -51,7 +52,7 @@ def convert_img(image_rgb):
 
     v = Variable(input_tensor, requires_grad=True)
     v = v.unsqueeze(0)
-    return v
+    return v.to(device)
 
 
 def get_visualization(path_to_image, cam_obj):
@@ -65,34 +66,40 @@ def get_visualization(path_to_image, cam_obj):
     return visualization
 
 
-images = [('/Users/przemyslawjablecki/PycharmProjects/FederatedLearning_MSc/segmented.png',
-           '/Users/przemyslawjablecki/PycharmProjects/FederatedLearning_MSc/full.png')]
+df = pd.read_csv('/net/archive/groups/plggsano/fl_msc_classification/rsna/test_labels_stage_1.csv')
+root_f = '/net/archive/groups/plggsano/fl_msc_classification/rsna/stage_1_test/full'
+root_s = '/net/archive/groups/plggsano/fl_msc_classification/rsna/stage_1_test/segmented'
+df['segmented'] = df['patient_id'].apply(lambda id: f"{root_s}/{id}.png")
+df['full'] = df['patient_id'].apply(lambda id: f"{root_f}/{id}.png")
+
+images = [(s, f, label) for s, f, label in zip(df['segmented'].values, df['full'].values, df['label'].values)]
+cam_resnet_segmented, resnet_segmented = get_resnet_cam(
+    '/net/archive/groups/plggsano/fl_msc_classification/iccs_models/d_rsna-segmented_m_ResNet50_r_10-c_3_bs_8_le_3_mf_3_ff_0.1_lr_0.0001_image_224_IID/best_model/ResNet50_7_acc_0.736_loss_0.624')
+cam_resnet_full, resnet_full = get_resnet_cam(
+    '/net/archive/groups/plggsano/fl_msc_classification/iccs_models/d_rsna-full_m_ResNet50_r_10-c_2_bs_8_le_3_mf_2_ff_0.1_lr_0.0001_image_224_IID/best_model/ResNet50_10_acc_0.757_loss_0.645')
+cam_densenet_segmented, densenet_segmented = get_densenet_cam(
+    '/net/archive/groups/plggsano/fl_msc_classification/iccs_models/d_rsna-segmented_m_DenseNet121_r_10-c_2_bs_8_le_3_mf_2_ff_0.1_lr_0.0001_image_224_IID/best_model/DenseNet121_7_acc_0.742_loss_0.65')
+cam_densenet_full, densenet_full = get_densenet_cam(
+    '/net/archive/groups/plggsano/fl_msc_classification/iccs_models/d_rsna-full_m_DenseNet121_r_10-c_3_bs_8_le_3_mf_3_ff_0.1_lr_0.0001_image_224_IID/best_model/DenseNet121_7_acc_0.737_loss_0.707')
 
 for image_path_tuple in images:
-    image_path_segmented, image_path_full = image_path_tuple
-    cam_resnet_segmented, resnet_segmented = get_resnet_cam(
-        '/Users/przemyslawjablecki/PycharmProjects/FederatedLearning_MSc/ResNet50_segmented')
-    cam_resnet_full, resnet_full = get_resnet_cam(
-        '/Users/przemyslawjablecki/PycharmProjects/FederatedLearning_MSc/ResNet50_full')
-    cam_densenet_segmented, densenet_segmented = get_densenet_cam(
-        '/Users/przemyslawjablecki/PycharmProjects/FederatedLearning_MSc/DenseNet121_segmented')
-    cam_densenet_full, densenet_full = get_densenet_cam(
-        '/Users/przemyslawjablecki/PycharmProjects/FederatedLearning_MSc/DenseNet121_full')
+    image_path_segmented, image_path_full, label = image_path_tuple
 
     converted_img_segmented = convert_img(get_image(image_path_segmented))
     converted_img_full = convert_img(get_image(image_path_full))
 
-    pred1 = np.argmax(resnet_segmented(converted_img_segmented).detach().numpy())
-    pred2 = np.argmax(densenet_segmented(converted_img_segmented).detach().numpy())
-    pred3 = np.argmax(resnet_full(converted_img_full).detach().numpy())
-    pred4 = np.argmax(densenet_full(converted_img_full).detach().numpy())
+    pred1 = np.argmax(resnet_segmented(converted_img_segmented).cpu().detach().numpy())
+    pred2 = np.argmax(densenet_segmented(converted_img_segmented).cpu().detach().numpy())
+    pred3 = np.argmax(resnet_full(converted_img_full).cpu().detach().numpy())
+    pred4 = np.argmax(densenet_full(converted_img_full).cpu().detach().numpy())
 
-    print(pred1, pred2, pred3, pred4)
-    pred_set = set([pred1, pred2, pred3, pred4])
-    if len(pred_set) != 1:
+    # print(pred1, pred2, pred3, pred4)
+    pred_set = {pred1, pred2, pred3, pred4}
+    if len(pred_set) != 1 or pred1 != label:
+        # print('Mismatch: ', label, pred_set)
         continue
-    classes = ["Normal", "No Lung Opacity / Not Normal", "Lung Opacity"]
-    print("Predicted class: ", classes[pred1])
+    classes = ["Normal", "No Lung Opacity", "Lung Opacity"]
+    # print("Predicted class: ", classes[pred1], label)
     vis1 = get_visualization(image_path_segmented, cam_resnet_segmented)
     vis2 = get_visualization(image_path_full, cam_resnet_full)
     vis3 = get_visualization(image_path_segmented, cam_densenet_segmented)
@@ -107,11 +114,17 @@ for image_path_tuple in images:
     fig4 = axarr[3].imshow(vis4)
 
     figs = [fig1, fig2, fig3, fig4]
-    for f in figs:
+    for f, description in zip(figs, ['ResNet50 (segmented)', 'ResNet50', 'DenseNet121 (segmented)', 'DenseNet121']):
         f.axes.get_xaxis().set_visible(False)
         f.axes.get_yaxis().set_visible(False)
-    plt.show()
+        f.axes.set_title(description, {'fontsize': 7, 'fontweight': '10'})
+    # plt.show()
+    if not os.path.exists('results'):
+        os.mkdir('results')
+    cls = classes[pred1].replace(' ', '_')
+    cls = classes[pred1].replace('/', '_')
+    plt.savefig(f'results/{os.path.basename(image_path_segmented)[:-4]}_{cls}.pdf', bbox_inches='tight')
+    plt.close()
+    et = time.time()
 
-et = time.time()
-
-print(et - st)
+    print(et - st)
