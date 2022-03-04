0fdb664cbd197044d68b54aed4aa381f229b17e0
Filip Ślazyk <filip.slazyk@gmail.com>
2022-03-04 13:24:01 +0100

    Fix conversion for CC-CXRI-P

--- classification/datasets/CC-CXRI-P/convert_to_beton.py
+++ classification/datasets/CC-CXRI-P/convert_to_beton.py
@@ -10,19 +10,19 @@ from utils import get_data_paths, CHESTDX_DATASET_PATH_BASE
 from ffcv.writer import DatasetWriter
 from ffcv.fields import RGBImageField, IntField
 
-images_dir, test_subset = get_data_paths('cc-cxri-p')
+images_dir, train_subset, test_subset = get_data_paths('cc-cxri-p')
 
-dataset = CCCXRIPDataset(-1, 1, test_subset, images_dir, -1)
-write_path = os.path.join(CHESTDX_DATASET_PATH_BASE, 'cc-cxri-p-test-jpg90.beton')
+for ds in [('cc-cxri-p-train-jpg90.beton', train_subset), ('cc-cxri-p-test-jpg90.beton', test_subset)]:
+    dataset = CCCXRIPDataset(-1, 1, ds[1], images_dir, -1)
+    write_path = os.path.join(CHESTDX_DATASET_PATH_BASE, ds[0])
 
-writer = DatasetWriter(write_path, {
-    'image': RGBImageField(
-        write_mode='jpg',
-        max_resolution=512,
-        jpeg_quality=90,
-    ),
-    'label': IntField()
-}, num_workers=24)
+    writer = DatasetWriter(write_path, {
+        'image': RGBImageField(
+            write_mode='jpg',
+            max_resolution=512,
+            jpeg_quality=90,
+        ),
+        'label': IntField()
+    }, num_workers=24)
 
-# Write dataset
-writer.from_indexed_dataset(dataset)
+    writer.from_indexed_dataset(dataset)
