a62de10d13d2ec0163394f932d0cf6aac9804bfd
Filip Ślazyk <filip.slazyk@gmail.com>
2022-03-18 14:56:13 +0100

    Prepare list of clients

--- classification/run_clients.sh
+++ classification/run_clients.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 echo "server node: $1"
+echo "server job id: $4"
 i=0
+touch "$4_client_ids.txt"
 
 while [ "$i" -lt "$2" ]; do
 	echo "starting $i/$2 client"
-	sbatch client_classification.sh $1 $i $2 $3
+	sbatch client_classification.sh $1 $i $2 $3 | cut -d " " -f 4 >> $4
 	i=$((i + 1))
 done
-
-
