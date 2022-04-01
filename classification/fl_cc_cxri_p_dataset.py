5c3ff50a14001a2f8a85fe43a84062f00c5570c6
Filip Ślazyk <filip.slazyk@gmail.com>
2022-04-01 16:16:09 +0200

    Fix length in CC-CXRI-P dataset

--- classification/fl_cc_cxri_p_dataset.py
+++ classification/fl_cc_cxri_p_dataset.py
@@ -14,12 +14,9 @@ class CCCXRIPDataset(Dataset):
         # "Normal"=0, "Viral"=1, "COVID"=2, "Other"=3
         self.classes_names = ["Normal", "Viral", "COVID", "Other"]
         self.images_file_df = pd.read_csv(images_file)
-        self.images_count = len(self.images_file_df)
 
         # IMAGES
         self.images = [os.path.join(images_dir, row['path']) for _, row in self.images_file_df.iterrows()]
-        print(f'Dataset file:{images_file}, len = {self.images_count}')
-
         # LABELS
         self.labels = [row['class'] for _, row in self.images_file_df.iterrows()]
 
@@ -31,6 +28,7 @@ class CCCXRIPDataset(Dataset):
         if client_id != -1:
             self.images, self.labels = selector.select_data(self.images, self.labels, client_id, clients_number)
         self.images_count = len(self.images)
+        print(f'Dataset file:{images_file}, len = {self.images_count}')
 
     def __len__(self):
         return self.images_count
