95ef7c425bdbb1f5a3a547a3b54f61164ff8acdd
Filip Ślazyk <filip.slazyk@gmail.com>
2022-05-19 17:51:29 +0200

    Update resize

--- classification/utils.py
+++ classification/utils.py
@@ -79,7 +79,7 @@ def get_train_transform_rsna(img_size):
 
 def get_test_transform_rsna(img_size):
     return torchvision.transforms.Compose([
-        torchvision.transforms.Resize(size=224),
+        torchvision.transforms.Resize(size=(224, 224)),
         torchvision.transforms.ToTensor(),
         torchvision.transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                          std=[0.229, 0.224, 0.225])
